8acfa27d1b9816854395db5320accec55bc77bea
weslei.bender <wesleimb@hotmail.com>
2024-01-10 15:46:01 -0300

    fix: create validations

--- src/main/java/br/com/weslei/bender/api/duelcards/domain/card/dto/request/CreateCardRequestDto.java
+++ src/main/java/br/com/weslei/bender/api/duelcards/domain/card/dto/request/CreateCardRequestDto.java
@@ -6,6 +6,7 @@ import br.com.weslei.bender.api.duelcards.domain.card.details.TrapCard;
 import br.com.weslei.bender.api.duelcards.domain.card.enumeration.CardRarity;
 import br.com.weslei.bender.api.duelcards.domain.card.enumeration.CardType;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -22,19 +23,20 @@ import lombok.NoArgsConstructor;
 public class CreateCardRequestDto {
 
     @NotBlank(message = "Nome não pode ser nulo!")
-    @Size(min = 1, max = 200)
+    @Size(min = 1, max = 200, message = "Nome: Tamanho deve ser entre 1 e 200")
     private String name;
 
     @NotBlank(message = "Descrição não pode ser nulo!")
-    @Size(min = 1, max = 500)
+    @Size(min = 1, max = 500, message = "Descrição: Tamanho deve ser entre 1 e 500")
     private String description;
 
-    @NotNull(message = "Tipo da carta não pode ser nulo!")
+    @NotNull(message = "Tipo da carta: Não pode ser nulo!")
     private CardType cardType;
 
-    @NotNull(message = "Raridade não pode ser nula!")
+    @NotNull(message = "Raridade da carta: Não pode ser nula!")
     private CardRarity cardRarity;
 
+    @Valid
     @JsonProperty(value = "monsterDetails")
     private MonsterCard monsterCard;
 
